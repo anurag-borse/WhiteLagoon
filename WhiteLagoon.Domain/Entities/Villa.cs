bf97c9b1ef810edfc536463c9eba77f9d692bd5d
Anurag <jai.borse01@gmail.com>
2024-07-23 18:02:20 +0530

    Villa Crud Completed

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
+        public string? ImageUrl { get; set; }
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Updated_Date { get; set; }
+
+
+
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+
+        }
+
+        public IActionResult Index()
+        {
+
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("", "Name and Description can not be the same");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                TempData["success"] = "Villa Added Successfully";
+
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "Villa Could Not be Added";
+            return View(villa);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+
+            var villaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 2).ToList();
+
+            if (villa == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Villa villa)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(villa);
+                _db.SaveChanges();
+                TempData["success"] = "Villa Updated Succesfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "Villa Could Not Be Updated";
+            return View(villa);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null)
+            {
+                TempData["error"] = "Villa Not Found";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            var villainDb = _db.Villas.FirstOrDefault(u => u.Id == villa.Id);
+
+            if (villainDb is not null)
+            {
+                _db.Villas.Remove(villainDb);
+                _db.SaveChanges();
+                TempData["success"] = "Villaa Deleted Successfully";
+                return RedirectToAction("Index", "Villa");
+
+            }
+            TempData["error"] = "Villa Could Not be Deleted";
+            return View();
+
+        }
+    }
+}
